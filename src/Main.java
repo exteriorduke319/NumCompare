a38abcb6165fb3c9c0287d02c7373ee29e79ef4f
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-28 09:41:39 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double num1;
+        double num2;
+
+        System.out.print("Please enter your first number: ");
+        if (scan.hasNextDouble()) {
+            num1 = scan.nextDouble();
+            System.out.print("Please enter your second number: ");
+            if (scan.hasNextDouble()) {
+                num2 = scan.nextDouble();
+                if (num1 > num2) {
+                    System.out.println(num1 + " is greater than " + num2);
+                } else if (num1 < num2) {
+                    System.out.println(num2 + " is greater than " + num1);
+                } else {
+                    System.out.println(num1 + " is equal to " + num2);
+                }
+            } else {
+                System.out.println("You entered an invalid value for your second number.  Please enter a number.");
+                System.exit(0);
+            }
+        } else {
+            System.out.println("You entered an invalid value for your first number.  Please enter a number.");
+            System.exit(0);
+        }
+
     }
 }
